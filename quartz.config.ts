fc479ac02cd7edf8791891a82f361f70db36368b
Pompitas <robertpomp9@gmail.com>
2025-11-18 14:10:33 +0100

    cambio locale a español
    cambio locale a español

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "es-ES",
     baseUrl: "speculum.damebrio.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
