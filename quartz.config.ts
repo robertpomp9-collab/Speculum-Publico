36315fbd328c0995b4eebde759ae42b22b671b2a
Pompitas <robertpomp9@gmail.com>
2025-11-18 12:57:43 +0100

    Crear portada y corregir URL
    Crear portada y corregir URL

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "robertpomp9-collab.github.io/Speculum-Publico",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
